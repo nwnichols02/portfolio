6db30c43c1040e901bf7deee709a83ed5392f6c3
Nate Nichols <nate.nichols@utili-source.com>
2026-02-23 21:57:33 -0600

    initial commit - after a while

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({mode}) => {
   const {GOOGLE_MAPS_API_KEY = ''} = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(GOOGLE_MAPS_API_KEY)
     },
